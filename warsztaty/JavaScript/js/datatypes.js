6f03471ccb0caa125211e86b19288841bf730c11
Konrad Firlej <kfirlej90@gmail.com>
2017-12-16 11:21:32 +0100

    JavaScript

---
+++ warsztaty/JavaScript/js/datatypes.js
@@ -0,0 +1,41 @@
+'use strict';
+
+//Typ liczbowy
+
+var pensja = 5000;
+var premia = 2000;
+var suma = pensja + premia;
+
+console.log(suma);
+
+//Typ łancuchowy (string)
+
+var pensjaStr ='5000';
+var premiaStr ='2500';
+var calkowitaKwotaStr = pensjaStr + premiaStr;
+
+console.log(calkowitaKwotaStr);
+
+//Typ logiczny (boolean)
+
+var czyJestSmog = true;
+console.log(czyJestSmog);
+
+//Typ specjalny (null i undefined)
+
+var nic; //undefined
+var nullnic = null; //null
+
+console.log(nic); //undefined
+console.log(nullnic); //null
+
+
+
+
+
+
+
+
+
+
+
--- warsztaty/JavaScript/js/variable-scope.js
+++ warsztaty/JavaScript/js/variable-scope.js
@@ -7,6 +7,29 @@ var iloscOsoba4 = 2500;
 
 var sumaKasy = iloscOsoba1 + iloscOsoba2 + iloscOsoba3;
 
+console.log('W głownym programie suma kasy: ' + sumaKasy);
+
+function policzSumeKasy(iloscOsoba1,iloscOsoba2,iloscOsoba3) {
+    //sumaKasy = 500;
+    var suma = iloscOsoba1 + iloscOsoba2 + iloscOsoba3;
+    
+    var sumaKasy = iloscOsoba1 +iloscOsoba2; //wyswietli 30 bo liczy zmienne lokalne w {}
+    
+    console.log('W sumie funkcji suma kasy: ' + sumaKasy);
+    console.log('W funkcji suma: ' + suma);
+    
+    return suma;
+}
+
+console.log(policzSumeKasy(10,20,30));
+
+//console.log('W glownym programie po wykonaniu funkcji suma kasy: ' + sumaKasy);
+
+
+
+
+
+
 
 
 
